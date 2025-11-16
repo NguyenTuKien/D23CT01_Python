69c7d8cf075eabe2c8cce8a6cacf441a7825adac
NguyenTuKien <KienNT.B23CN465@stu.ptit.edu.vn>
2025-11-16 14:50:53 +0700

    Update

---
+++ Codeptit/PYKT088.py
@@ -0,0 +1,11 @@
+from sys import stdin, stdout
+
+
+def main():
+    
+    data = stdin.buffer.read().split()
+    it = iter(data)
+    # for _ in range(int(next(it))):
+
+if __name__ == "__main__":
+    main()
